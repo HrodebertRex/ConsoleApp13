3bfa7b1f028b64b45eaa44345a9b78a72f351d0f
_ArcLight_ <kakabadzerobert@gmail.com>
2024-03-26 03:23:38 +0400

    Добавьте файлы проекта.

---
+++ ConsoleApp13/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.Design;
+
+class Programm
+{
+    static void Main(string[] args)
+    {
+        int age;
+
+        Console.WriteLine("Введите возраст пользователя");
+
+        age = int.Parse(Console.ReadLine());
+
+        if (age <= 17)
+        {
+            Console.WriteLine($"{age}...да ты ещё пездюк >.<");
+        }
+        else if (age >= 18 && age <= 100)
+        {
+            Console.WriteLine("Добро пожаловать!");
+        }
+        else 
+        {
+            Console.WriteLine("Столько не живут UwU");
+        }
+
+
+    }
+}
